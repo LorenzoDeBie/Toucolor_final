39b66f311a88f4aafb68c8e923daeeb287b07381
VinceNaessens <vince.naessens01@gmail.com>
2017-05-10 19:13:48 +0200

    poep

--- src/Toucolor/Actor.java
+++ src/Toucolor/Actor.java
@@ -78,10 +78,9 @@ class Actor {
                                 fullUpdateY = fullCoords[1][1] + blockSize;
                                 cancelJump = true;
                             } else {
-                                fullUpdateY = actorY;
+                                fullUpdateY = fullCoords[6][1] - blockSize;
                                 isInAir = false;
                             }
-
                         } else {
                             if (jumping) {
                                 fullUpdateY = fullCoords[1][1] + blockSize;
